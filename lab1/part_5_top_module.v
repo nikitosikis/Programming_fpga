618849713d25d3b5db0237c809513b826cc83863
Nick <ibanov-nikit04@mail.ru>
2025-10-07 22:20:32 +0300

    правки 4 и 5 заданий второй части

--- lab1/part_5_top_module.v
+++ lab1/part_5_top_module.v
@@ -7,10 +7,11 @@ module part_5_top_module (
     
     wire cout_lower; 
     wire [15:0] b_lower;
+    wire [15:0] b_upper;
 
     assign b_lower = b[15:0] ^ {16{sub}};
+    assign b_upper = b[31:16] ^ {16{sub}};
     
-
     add16 lower16 (
         .a(a[15:0]),
         .b(b_lower),
@@ -19,17 +20,15 @@ module part_5_top_module (
         .cout(cout_lower)
     );
     
-
     add16 upper16 (
         .a(a[31:16]),
-        .b(b[31:16]),
+        .b(b_upper),
         .cin(cout_lower),
         .sum(sum[31:16]),
         .cout() 
     );
 
 endmodule
-
 
 module add16 (
     input [15:0] a,
@@ -40,3 +39,4 @@ module add16 (
 );
     assign {cout, sum} = a + b + cin;
 endmodule
+
